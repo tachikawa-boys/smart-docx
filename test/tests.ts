1dfab48f11bddd3105db114c705e01bbad58f4ec
silkwaffle <coker.aaryn@gmail.com>
2020-06-10 10:18:52 +0900

    Dev/wordfiles azc (#6)
    * Commiting to troubleshoot arbitrary file names, arbitrary strings. Currently having issues.
    
    * Fixed issues with testing arbitrary file names, arbitrary paragraphs.
    
    Co-authored-by: Steve Brower <sbrower-dev@outlook.com>

--- test/tests.ts
+++ test/tests.ts
@@ -7,6 +7,8 @@ import { unlink } from "fs";
 import { tmpdir } from 'tmp';
 import { DocReader } from "../ts-src/DocReader";
 import { DocWriter } from "../ts-src/DocWriter";
+
+import randomstring = require('randomstring');
 import { Paragraph } from 'docx';
 import { Header } from 'docx/build/file/header/header';
 
@@ -28,31 +30,40 @@ async function cleanupTmp(expectedFile: string) {
     });
 }
 
+
+
 describe("Basic Unit Tests", () => {
     it("Create docx", async () => {
 
+        const randFileName = randomstring.generate(7);
+        const randString = randomstring.generate();
+
         const temp = tmpdir;
-        const doc = new DocWriter("Test", "TestDesc", "TestFile", "Test");
+        const doc = new DocWriter("Test", "TestDesc", randFileName, "Test");
         doc.registerHandle(temp);
         await doc.closeHandle();
 
-        const expectedFile = temp + '\\TestFile.docx';
+        //const expectedFile = temp + '\\TestFile.docx';
+        const expectedFile = temp + '\\' + randFileName + '.docx';
         expect(file(expectedFile)).to.exist;
         await cleanupTmp(expectedFile);
     });
 
     it("Simple Write no Verify", async () => {
+        const randFileName = randomstring.generate(7);
+        const randString = randomstring.generate();
         const temp = tmpdir;
-        const doc = new DocWriter("Test", "TestDesc", "TestFile", "Test");
+        const doc = new DocWriter("Test", "TestDesc", randFileName, "Test");
         doc.registerHandle(temp);
 
-        const paragraph1 = new docx.Paragraph("HelloWorld!");
+        const paragraph1 = new docx.Paragraph(randString);
         const paragraphs: docx.Paragraph[] = new Array(paragraph1);
 
         doc.writeSection(paragraphs, null, null);
         await doc.closeHandle();
 
-        const expectedFile = temp + '\\TestFile.docx';
+        //const expectedFile = temp + '\\TestFile.docx';
+        const expectedFile = temp + '\\' + randFileName + '.docx';
         await cleanupTmp(expectedFile);
     });
 });
@@ -60,43 +71,52 @@ describe("Basic Unit Tests", () => {
 describe("Read/Write Unit Tests", () => {
 
     it("Simple Write Verify", async () => {
+        const randFileName = randomstring.generate(7);
+        const randString = randomstring.generate();
         const temp = tmpdir;
-        const doc = new DocWriter("Test", "TestDesc", "TestFile", "Test");
+        const doc = new DocWriter("Test", "TestDesc", randFileName, "Test");
         doc.registerHandle(temp);
 
-        const paragraph1 = new docx.Paragraph("HelloWorld!");
+        const paragraph1 = new docx.Paragraph(randString);
         const paragraphs: docx.Paragraph[] = new Array(paragraph1);
 
         doc.writeSection(paragraphs, null, null);
         await doc.closeHandle();
 
-        const expectedFile = temp + '\\TestFile.docx';
+        //const expectedFile = temp + '\\TestFile.docx';
+        const expectedFile = temp + '\\' + randFileName + '.docx';
         expect(file(expectedFile)).to.exist;
         const docR = new DocReader(expectedFile);
 
         docR.openDoc();
-        expect(docR.searchForBodyText("HelloWorld!")).to.be.true;
+        expect(docR.searchForBodyText(randString)).to.be.true;
         await docR.closeDoc();
         await cleanupTmp(expectedFile);
     });
 
     it("Simple Write Verify (Fail)", async () => {
+        const randFileName = randomstring.generate(7);
+        const randString = randomstring.generate();
+        const randStringFail = randomstring.generate(31);
         const temp = tmpdir;
-        const doc = new DocWriter("FailTest", "TestDesc", "FailureTestFile", "Test");
+        const doc = new DocWriter("Test", "TestDesc", randFileName, "Test");
+
         doc.registerHandle(temp);
 
-        const paragraph1 = new docx.Paragraph("Hello World!");
+        const paragraph1 = new docx.Paragraph(randString);
         const paragraphs: docx.Paragraph[] = new Array(paragraph1);
 
         doc.writeSection(paragraphs, null, null);
         await doc.closeHandle();
 
-        const expectedFile = temp + '\\FailureTestFile.docx';
+        //const expectedFile = temp + '\\TestFile.docx';
+        const expectedFile = temp + '\\' + randFileName + '.docx';
+
         expect(file(expectedFile)).to.exist;
         const docR = new DocReader(expectedFile);
 
         docR.openDoc();
-        expect(docR.searchForBodyText("HelloWorld!")).to.be.false;
+        expect(docR.searchForBodyText(randStringFail)).to.be.false;
         await docR.closeDoc();
         await cleanupTmp(expectedFile);
     });
